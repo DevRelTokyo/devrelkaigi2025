32a8897eaa579fdae4bb9e27e3e860fa18d518c6
Atsushi Nakatsugawa <atsushi@moongift.co.jp>
2025-06-30 22:33:01 +0900

    Add ticket table

--- app/components/priceTable.tsx
+++ app/components/priceTable.tsx
@@ -1,11 +1,12 @@
 import { useParams } from "@remix-run/react";
 import { setLang } from "../utils/i18n";
+import { Link } from "@remix-run/react";
 
 export default function PriceTable() {
   const params = useParams();
   const { locale } = params;
   const { t } = setLang(locale!);
-  const col = locale === 'ja' ? 4 : 4;
+  const col = locale === "ja" ? 4 : 4;
   return (
     <section className="section pricing">
       <div className="container">
@@ -14,167 +15,268 @@ export default function PriceTable() {
             <div className="section-title">
               <h3
                 dangerouslySetInnerHTML={{
-                  __html: t('Get <span class="alternate">ticket</span>')
+                  __html: t('Get <span class="alternate">ticket</span>'),
                 }}
               ></h3>
+              <p>{t("Our tickets are separate by features")}</p>
               <p>
-                {t('Our tickets are separa')}
+                <Link to={`/${locale}/contact?category=invoice`}>
+                  {t("We support invoice payment. Please contact us here")}
+                </Link>
               </p>
             </div>
           </div>
         </div>
         <div className="row">
-          {locale === 'ja' &&
+          {locale === "ja" && (
             <>
               <div className={`col-md-${col}`}>
                 <div className="pricing-item">
                   <div className="pricing-heading">
                     <div className="title">
-                      <h6>{t('Workshop')}</h6>
+                      <h6>{t("Workshop")}</h6>
                     </div>
                     <div className="price">
                       <h2
                         dangerouslySetInnerHTML={{
-                          __html: t('<span>$</span>39.00')
+                          __html: t("<span>$</span>50.00"),
                         }}
                       ></h2>
                     </div>
                   </div>
                   <div className="pricing-body">
-
                     <ul className="feature-list m-0 p-0">
-                      <li><p><span className="fa fa-check-circle available"></span>{t('Workshop on 2nd Oct')}</p></li>
-                      <li><p><span className="fa fa-check-circle available"></span>{t('Drinks and Refreshments')}</p></li>
-                      <li><p><span className="fa fa-check-circle available"></span>{' '}</p></li>
-                      <li><p><span className="fa fa-check-circle available"></span>{' '}</p></li>
+                      <li>
+                        <p>
+                          <span className="fa fa-check-circle available"></span>
+                          {t("Workshop at 1PM to 5PM on 2nd Oct")}
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span className="fa fa-check-circle available"></span>
+                          {t("Drinks and Refreshments on 2nd Oct")}
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span className="fa fa-check-circle available"></span>{" "}
+                        </p>
+                      </li>
                     </ul>
                   </div>
                   <div className="pricing-footer text-center">
-                    <a href="/" className="btn btn-transparent-md">Buy a ticket</a>
+                    <a
+                      href="https://ti.to/devrelkaigi/2025/with/japanese"
+                      target="_blank"
+                      className="btn btn-transparent-md"
+                    >
+                      {t("Buy a ticket")}
+                    </a>
                   </div>
                 </div>
               </div>
             </>
-          }
+          )}
           <div className={`col-md-${col}`}>
-
             <div className="pricing-item">
               <div className="pricing-heading">
-
                 <div className="title">
-                  <h6>{t('A day ticket')}</h6>
+                  <h6>{t("Sessions ticket")}</h6>
                 </div>
 
                 <div className="price">
                   <h2
                     dangerouslySetInnerHTML={{
-                      __html: t('<span>$</span>39.00')
+                      __html: t("<span>$</span>20.00"),
                     }}
                   ></h2>
                 </div>
               </div>
               <div className="pricing-body">
-
                 <ul className="feature-list m-0 p-0">
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Every sessions on a day')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Attend on 3rd or 4th Oct')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Lunch')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Drinks and Refreshments')}</p></li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Every sessions on 3rd and 4th Oct")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Refreshments and Drinks on 3rd and 4th Oct")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>{" "}
+                    </p>
+                  </li>
                 </ul>
               </div>
               <div className="pricing-footer text-center">
-                <a href="/" className="btn btn-transparent-md">Buy a ticket</a>
+                <a
+                  href="https://ti.to/devrelkaigi/2025/with/only-sessions-3-4"
+                  className="btn btn-transparent-md"
+                  target="_blank"
+                >
+                  {t("Buy a ticket")}
+                </a>
               </div>
             </div>
           </div>
           <div className={`col-md-${col}`}>
-
             <div className="pricing-item">
               <div className="pricing-heading">
-
                 <div className="title">
-                  <h6>{t('2 days ticket')}</h6>
+                  <h6>{t("Sessions & Lunch")}</h6>
                 </div>
 
                 <div className="price">
                   <h2
                     dangerouslySetInnerHTML={{
-                      __html: t('<span>$</span>39.00')
+                      __html: t("<span>$</span>30.00"),
                     }}
                   ></h2>
                 </div>
               </div>
               <div className="pricing-body">
                 <ul className="feature-list m-0 p-0">
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Every sessions on two days')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Attend on 3rd and 4th Oct')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Lunch')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Drinks and Refreshments')}</p></li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Every sessions on 3rd and 4th Oct")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Refreshments and Drinks")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Lunch on 4th Oct")}
+                    </p>
+                  </li>
                 </ul>
               </div>
               <div className="pricing-footer text-center">
-                <a href="/" className="btn btn-transparent-md">Buy a ticket</a>
+                <a
+                  href="https://ti.to/devrelkaigi/2025/with/session-lunch"
+                  className="btn btn-transparent-md"
+                  target="_blank"
+                >
+                  {t("Buy a ticket")}
+                </a>
               </div>
             </div>
           </div>
           <div className={`col-md-${col}`}>
-
             <div className="pricing-item">
               <div className="pricing-heading">
-
                 <div className="title">
-                  <h6>{t('A day + 🎉 ticket')}</h6>
+                  <h6>{t("Sessions & after party")}</h6>
                 </div>
 
                 <div className="price">
                   <h2
                     dangerouslySetInnerHTML={{
-                      __html: t('<span>$</span>39.00')
+                      __html: t("<span>$</span>50.00"),
                     }}
                   ></h2>
                 </div>
               </div>
               <div className="pricing-body">
-
                 <ul className="feature-list m-0 p-0">
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Including a day ticket')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('After party on 4th Oct')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{' '}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{' '}</p></li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Every sessions on 3rd and 4th Oct")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Refreshments and Drinks")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("After party on 4th Oct")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>{" "}
+                    </p>
+                  </li>
                 </ul>
               </div>
               <div className="pricing-footer text-center">
-                <a href="/" className="btn btn-transparent-md">Buy a ticket</a>
+                <a
+                  href="https://ti.to/devrelkaigi/2025/with/xcvl1zeba8m"
+                  className="btn btn-transparent-md"
+                  target="_blank"
+                >
+                  {t("Buy a ticket")}
+                </a>
               </div>
             </div>
           </div>
           <div className={`col-md-${col}`}>
-
             <div className="pricing-item">
               <div className="pricing-heading">
                 <div className="title">
-                  <h6>{t('2 days + 🎉 ticket')}</h6>
+                  <h6>{t("Full combo")}</h6>
                 </div>
 
                 <div className="price">
                   <h2
                     dangerouslySetInnerHTML={{
-                      __html: t('<span>$</span>39.00')
+                      __html: t("<span>$</span>60.00"),
                     }}
                   ></h2>
                 </div>
               </div>
               <div className="pricing-body">
-
                 <ul className="feature-list m-0 p-0">
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Including two days ticket')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('After party on 4th Oct')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{' '}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{' '}</p></li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Every sessions on 3rd and 4th Oct")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Refreshments and Drinks")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Lunch on 4th Oct")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("After party on 4th Oct")}
+                    </p>
+                  </li>
                 </ul>
               </div>
               <div className="pricing-footer text-center">
-                <a href="/" className="btn btn-transparent-md">Buy a ticket</a>
+                <a
+                  href="https://ti.to/devrelkaigi/2025/with/full-combo"
+                  className="btn btn-transparent-md"
+                  target="_blank"
+                >
+                  {t("Buy a ticket")}
+                </a>
               </div>
             </div>
           </div>
@@ -182,28 +284,52 @@ export default function PriceTable() {
             <div className="pricing-item featured">
               <div className="pricing-heading">
                 <div className="title">
-                  <h6>{t('Full combo ticket')}</h6>
+                  <h6>{t("Personal Sponsor")}</h6>
                 </div>
 
                 <div className="price">
                   <h2
                     dangerouslySetInnerHTML={{
-                      __html: t('<span>$</span>39.00')
+                      __html: t("<span>$</span>150.00"),
                     }}
                   ></h2>
                 </div>
               </div>
               <div className="pricing-body">
-
                 <ul className="feature-list m-0 p-0">
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Including two days ticket')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('Workshop on 2nd Oct')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{t('After party on 4th Oct')}</p></li>
-                  <li><p><span className="fa fa-check-circle available"></span>{' '}</p></li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Including full combo ticket")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("Speaker dinner on 2nd Oct")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>
+                      {t("List your name on the website")}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="fa fa-check-circle available"></span>{" "}
+                    </p>
+                  </li>
                 </ul>
               </div>
               <div className="pricing-footer text-center">
-                <a href="/" className="btn btn-main-md">Buy a ticket</a>
+                <a
+                  href="https://ti.to/devrelkaigi/2025/with/personal-sponsor"
+                  className="btn btn-main-md"
+                  target="_blank"
+                >
+                  {t("Buy a ticket")}
+                </a>
               </div>
             </div>
           </div>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,15 @@
-import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
 import { ServerRuntimeMetaArgs } from "@remix-run/server-runtime";
 import About from "~/components/about";
 import Banner from "~/components/banner";
 import FooterMain from "~/components/footerMain";
 import FooterSub from "~/components/footerSub";
 import Navi from "~/components/navi";
-import Price from "~/components/price";
+import Price from "~/components/priceTable";
 import Schedule from "~/components/schedule";
 import Speakers from "~/components/speakers";
 import Sponsors from "~/components/sponsors";
@@ -20,18 +24,17 @@ interface MetaProps {
 }
 export const meta: MetaFunction = ({ data }: ServerRuntimeMetaArgs) => {
   const { title, description } = data as MetaProps;
-  return [
-    { title },
-    { name: "description", content: description },
-  ];
+  return [{ title }, { name: "description", content: description }];
 };
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { locale } = params;
   const { t } = setLang(locale!);
   return json({
-    title: t('DevRelKaigi 2025'),
-    description: t("DevRelKaigi is an international conference of developer relations from Tokyo with ❤️."),
+    title: t("DevRelKaigi 2025"),
+    description: t(
+      "DevRelKaigi is an international conference of developer relations from Tokyo with ❤️."
+    ),
   });
 }
 
